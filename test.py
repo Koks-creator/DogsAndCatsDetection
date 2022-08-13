6e40b1a45aa814faf76885056214e8bba1e510ba
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2022-08-13 12:28:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,12 @@ import os
 import cv2
 import numpy as np
 from tensorflow import keras
+import matplotlib.pyplot as plt
 
 classes = ["cat", "dog"]
 model = keras.models.load_model('CatsDogsModel.h5')
 
+images = []
 for image_name in os.listdir("images"):
     # print(image_name)
     img = cv2.imread(r"images/" + image_name)
@@ -19,6 +21,13 @@ for image_name in os.listdir("images"):
     print(f"{image_name}: {classes[label]}")
 
     cv2.putText(img, f"Class: {classes[label]}", (5, 25), cv2.FONT_HERSHEY_PLAIN, 2, (200, 20, 50), 2)
-    cv2.imshow(image_name, img)
-    cv2.waitKey(0)
+    images.append(img)
+    # cv2.imshow(image_name, img)
+    # cv2.waitKey(0)
+
+for i in range(18):
+    plt.subplot(3, 6, i + 1)
+    plt.imshow(images[i], cmap='gray')
+plt.show()
+
 
